f528453356688dae01ac0437bf8f59caf4efdaa4
Alan Deverill <alanpd36@gmail.com>
2022-05-30 15:17:45 +0100

    Starting with Twig to render a response

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\EventListener\ResponseListener;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController 
+class VinylController extends AbstractController
 
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['song' => 'Get down on it', 'artist' => 'Kool and the Gang'],
+            ['song' => 'Loving you', 'artist' => 'Elvis Presley'],
+            ['song' => 'Hey Laura', 'artist' => 'Gregory Porter'],
+            ['song' => 'Mr Holden', 'artist' => 'Gregory Porter'],
+            ['song' => 'Raining in my Heart', 'artist' => 'Buddy Holly'],
+            ['song' => 'Make Someone Happy', 'artist' => 'Jeff Goldblum'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'Rock & Jazz smooth',
+            'tracks' => $tracks,
+        ]);
     }
     #[Route('/browse/{slug}')]
     public function browse(string $slug = null): Response

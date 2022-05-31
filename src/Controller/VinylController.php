956374e9d1bcee334744892aaead35395afc635c
Alan Deverill <alanpd36@gmail.com>
2022-05-31 20:19:02 +0100

    updated brows added css assets

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,40 +2,41 @@
 
 namespace App\Controller;
 
+use function Symfony\Component\String\u;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\EventListener\ResponseListener;
 use Symfony\Component\Routing\Annotation\Route;
-use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
-
 {
     #[Route('/')]
-    public function homepage(): Response
+public function homepage(): Response
     {
-        $tracks = [
-            ['song' => 'Get down on it', 'artist' => 'Kool and the Gang'],
-            ['song' => 'Loving you', 'artist' => 'Elvis Presley'],
-            ['song' => 'Hey Laura', 'artist' => 'Gregory Porter'],
-            ['song' => 'Mr Holden', 'artist' => 'Gregory Porter'],
-            ['song' => 'Raining in my Heart', 'artist' => 'Buddy Holly'],
-            ['song' => 'Make Someone Happy', 'artist' => 'Jeff Goldblum'],
-        ];
+    $tracks = [
+        ['song' => 'Get down on it', 'artist' => 'Kool and the Gang'],
+        ['song' => 'Loving you', 'artist' => 'Elvis Presley'],
+        ['song' => 'Hey Laura', 'artist' => 'Gregory Porter'],
+        ['song' => 'Mr Holden', 'artist' => 'Gregory Porter'],
+        ['song' => 'Raining in my Heart', 'artist' => 'Buddy Holly'],
+        ['song' => 'Make Someone Happy', 'artist' => 'Jeff Goldblum'],
+        ['song' => 'Anything Goes', 'artist' => 'Harry Connick Jr'],
+        ['song' => 'Step on my old Size nines', 'artist' => 'Stereophonics'],
+        ['song' => 'Star', 'artist' => 'Brian Adams'],
+        ['song' => 'Woke up this morning', 'artist' => 'Nazarath'],
+    ];
 
-        return $this->render('vinyl/homepage.html.twig', [
-            'title' => 'Rock & Jazz smooth',
-            'tracks' => $tracks,
-        ]);
-    }
-    #[Route('/browse/{slug}')]
-    public function browse(string $slug = null): Response
+    return $this->render('vinyl/homepage.html.twig', [
+        'title' => 'Rock & Jazz smooth',
+        'tracks' => $tracks,
+    ]);
+}
+#[Route('/browse/{slug}')]
+public function browse(string $slug = null): Response
     {
-        if ($slug) {
-            $title = 'Genre: ' .u(str_replace('-', ' ', $slug))->title(true);
-        } else {
-            $title = 'All Genres';
-        return new Response($title);
-        }
-    }
+    
+    $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+    return $this->render('vinyl/browse.html.twig', [
+        'genre' => $genre,
+    ]);
+}
 }
